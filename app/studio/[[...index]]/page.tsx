8b2f19dc00689fd89702c269093a58e5c72f8e8a
bojianlee19-design <bojianlee19@gmail.com>
2025-09-01 07:41:25 +0800

    Update page.tsx

--- app/studio/[[...index]]/page.tsx
+++ app/studio/[[...index]]/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { NextStudio } from 'next-sanity/studio'
-import config from '../../../sanity.config'
+import config from '../../../sanity.config' // 注意这条相对路径
+
 export default function StudioPage() {
   return <NextStudio config={config} />
 }
