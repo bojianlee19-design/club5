21f1f8c758a0417ea651a61a41c0e96a9fe79d87
bojianlee19-design <bojianlee19@gmail.com>
2025-09-02 10:00:30 +0800

    Update sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,14 +1,17 @@
+// sanity.config.ts
 import { defineConfig } from 'sanity'
 import { deskTool } from 'sanity/desk'
 import { visionTool } from '@sanity/vision'
-import schemas from './sanity/schemas'
+import { schemaTypes } from './sanity/schemas' // ← 用命名导入
 
 export default defineConfig({
   name: 'hazyclub',
-  title: 'Hazy Club CMS',
+  title: 'HAZY Club Studio',
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
-  basePath: '/studio', // 后台入口
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
+  basePath: '/studio',
   plugins: [deskTool(), visionTool()],
-  schema: { types: schemas }
+  schema: {
+    types: schemaTypes, // ← 使用命名导出的 schemaTypes
+  },
 })
